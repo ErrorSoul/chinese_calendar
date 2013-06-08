1a45ae556e099f081e869906dfb6eaf0b26e2402
Error Soul <neurofag@gmail.com>
2013-06-08 22:11:49 +0400

    tool without method month_converter() in Date() class

--- tools.py
+++ tools.py
@@ -32,12 +32,20 @@ class Cycle(object):
                       "yang metal","ying metal","yang water","ying water")
 
     # method of Ppsckovskiy D.V.
-    # moon status 
+    # moon status for date (B)
+    # B = Bo + number of month + count of days
+    # Bo = moon age  on 30Th November of last year
+    # moon age of newmoon equal 0
+    # {year in Methon's cycle : Bo}
+    
+
     table_year = {0: 27, 1: 8, 2: 19, 3: 0, 4: 11, 5: 22, 6: 3, 7: 14,
-                  8: 25, 9: 6, 10: 17,11: 28, 12: 9,13: 20,14: 2,15: 13,
+                  8: 25, 9: 6, 10: 17,11: 28, 12: 9,13: 20,14: 2 ,15: 13,
                   16: 24, 17: 5,18: 16}
 
 
+    first_month = 1
+    second_month = 2
     heavtocycle = heavenly_stems * 5
     earthtocycle = earth_branches * 6
     comb_dict = {x:y for x,y in enumerate(zip(heavtocycle,earthtocycle))}
@@ -56,12 +64,35 @@ class Date(datetime, Cycle):
         
         #new cycle of hours started in 16 dec 1923 23:00
         self.start_hour = datetime(1923,12,16, 23, 0)
-        
+        self.moon_month = 30
+        self.b = self.table_year[self.year % 19]
+        self.year_new_moon = datetime(self.year,self.first_month,
+                                      self.moon_month - self.first_month
+                                      - self.b)
 
     def convert_year(self):
         """ Return chinese representation of year """
 
-        return self.comb_dict[(self.year - 1924) % 60]
+       
+        
+        # 22 jan the most earliest date of chinese new year
+        if  self.year_new_moon >= datetime(self.year, self.first_month, 22):
+            moon_new_year = self.year_new_moon
+            print 'moon_new_year', moon_new_year 
+        else:
+            moon_new_year = datetime(self.year,self.second_month,
+                                     self.moon_month - self.second_month
+                                     - self.b )
+            print 'moon_new_year', moon_new_year 
+
+        if moon_new_year <= self:
+            num = (self.year - 1924) % 60
+        else:
+            num = ((self.year - 1924) % 60) - 1
+            
+        
+
+        return self.comb_dict[num]
 
     def convert_month(self):
         """ Return chinese representation of month """
@@ -87,12 +118,6 @@ class Date(datetime, Cycle):
         
 
 
-
-
-        
-
-        
-    
 
 
 
